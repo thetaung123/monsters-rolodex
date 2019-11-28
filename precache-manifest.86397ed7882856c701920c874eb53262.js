33546fb964c592cf7686411ed673a9deabb89fc9
Thet <zarnee96@gmail.com>
2019-11-28 21:34:32 +0630

    Updates

---
+++ precache-manifest.86397ed7882856c701920c874eb53262.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9519a4f123d897534d9652ad47b4b30a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a7276cb90ef7efbba097",
+    "url": "/monsters-rolodex/static/css/main.c663c6c1.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "a7276cb90ef7efbba097",
+    "url": "/monsters-rolodex/static/js/main.b3cc06bb.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
